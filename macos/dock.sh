e98dde12d87fc8291b6c305415634e735abcd221
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-03-22 02:02:03 +0700

    chore: fix workflow

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,7 +3,6 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Joplin.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Google Chrome Canary.app"
 dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
