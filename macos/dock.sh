6f67cee2d75c551ff22e75247daa1218ae313fb4
Jirawat Boonkumnerd <jo06942@gmail.com>
2024-07-04 13:27:01 +0700

    fix: docutils

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,10 +3,8 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Joplin.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-dockutil --no-restart --add "/System/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/System/Applications/System Settings.app"
 
 killall Dock
